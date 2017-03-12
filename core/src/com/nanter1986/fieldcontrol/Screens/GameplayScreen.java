11a87d67dfa87702fcbd1025f27df79a101214be
nanter1986 <nanter1986@gmail.com>
2017-03-12 17:11:24 +0200

    movement

--- core/src/com/nanter1986/fieldcontrol/Screens/GameplayScreen.java
+++ core/src/com/nanter1986/fieldcontrol/Screens/GameplayScreen.java
@@ -1,11 +1,9 @@
 package com.nanter1986.fieldcontrol.Screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.nanter1986.fieldcontrol.AdsController;
 import com.nanter1986.fieldcontrol.DisplayToolkit;
 import com.nanter1986.fieldcontrol.FieldControl;
@@ -66,34 +64,7 @@ public class GameplayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        for (Pawn p : pawns) {
-            if (p.isTouched() && p.selected) {
-                customLog("here");
-                p.selected = false;
-                actionSelect = false;
-                break;
-            } else if (p.isTouched()) {
-                customLog("here2");
-                p.selected = true;
-                actionSelect = true;
-                break;
-            } else if (actionSelect) {
-                for (FloorTile f : floor) {
-                    if (f.isButtonTouched()) {
-                        if (p.selected) {
-                            customLog("here3");
-                            p.positionX = f.buttonX;
-                            p.positionY = f.buttonY;
-                            p.selected = false;
-                            actionSelect = false;
-                        }
-
-                    }
-                }
-            }
-        }
-
-
+        controlCheck();
         tool.camera.update();
         Gdx.gl.glClearColor(BACKGROUND_COLOR.r, BACKGROUND_COLOR.g,
                 BACKGROUND_COLOR.b, BACKGROUND_COLOR.a);
@@ -107,6 +78,61 @@ public class GameplayScreen implements Screen {
             p.appear();
         }
         tool.batch.end();
+    }
+
+    private void controlCheck() {
+        for (Pawn p : pawns) {
+            if (p.isTouched() && p.selected) {
+                customLog("here");
+                p.selected = false;
+                actionSelect = false;
+                break;
+            } else if (p.isTouched()&&actionSelect == false) {
+                customLog("here2");
+                p.selected = true;
+                actionSelect = true;
+                break;
+            } else if (p.selected) {
+                for (FloorTile f : floor) {
+                    if (f.isButtonTouched()) {
+                        if (checkIfTileIsFree(f) && checkIfCloseEnough(f,p)) {
+                            p.positionX = f.buttonX;
+                            p.positionY = f.buttonY;
+                            p.selected = false;
+                            actionSelect = false;
+                        }
+
+
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean checkIfCloseEnough(FloorTile f,Pawn p) {
+        boolean closeEnough=false;
+        if(p.speed>=Math.abs(p.positionX-f.buttonX) && p.speed>=Math.abs(p.positionY-f.buttonY)){
+            if(p.positionX==f.buttonX || p.positionY==f.buttonY){
+                closeEnough=true;
+            }
+        }else{
+            customLog("cant reach");
+        }
+        return closeEnough;
+    }
+
+    private boolean checkIfTileIsFree(FloorTile f) {
+        boolean free = true;
+        for (Pawn p : pawns) {
+            if (f.buttonX == p.positionX && f.buttonY == p.positionY) {
+                free = false;
+                break;
+            }else {
+                customLog("Cell not free");
+            }
+        }
+
+        return free;
     }
 
     private void makeFloor() {
